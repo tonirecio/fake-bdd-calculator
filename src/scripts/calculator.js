3a120cc3be9b394af642865accac6a8b451b4d50
Recio, Toni <Toni.Recio@travelport.com>
2023-06-17 12:56:15 +0200

    Setting up Linting

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,31 +1,19 @@
-/* eslint-disable no-alert */
-/* eslint-disable operator-linebreak */
-/* eslint-disable indent */
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-/* eslint-disable default-case */
+const MAX_DIGITS_IN_DISPLAY = 10
 
-const MAX_DIGITS_IN_DISPLAY = 10;
-
-let display;
-
-function setDisplay(value) {
-	display.innerHTML = '0,';
+const setDisplay = (value) => {
+  display.innerHTML = 'INTERNS'
 }
 
-function sayHello() {
-	window.alert('Hello');
+const sayHello = () => {
+  window.alert('Hello. The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.')
 }
 
-function reset() {
-	setDisplay(0);
+const reset = () => {
+  setDisplay(0)
 }
 
-function inicialize() {
-	display = document.querySelector('div[name="display"] span');
-	document.getElementsByName('multiply')[0].addEventListener('click', () => {
-		sayHello();
-	});
-	reset();
-}
+const display = document.querySelector('div[name="display"] span')
+document.getElementsByName('multiply')[0].addEventListener('click', () => {
+  sayHello()
+})
+reset()
--- tests/steps/calculator.steps.js
+++ tests/steps/calculator.steps.js
@@ -1,69 +1,68 @@
-/* eslint-disable no-undef */
-const { Given, When, Then } = require('@cucumber/cucumber');
-const { expect } = require('@playwright/test');
+const { Given, When, Then } = require('@cucumber/cucumber')
+const { expect } = require('@playwright/test')
 
-const url = 'http://127.0.0.1:5500/src/calculator.html';
+const url = 'http://127.0.0.1:5500/src/calculator.html'
 
-async function buttonClick(buttonId) {
-	await page.click(`[data-testid="${buttonId}"]`, { force: true });
+async function buttonClick (buttonId) {
+  await page.click(`[data-testid="${buttonId}"]`, { force: true })
 }
 
-async function serialForEach(array, cb) {
-	return array.reduce((promise, data) => promise.then(() => cb(data)), Promise.resolve());
+async function serialForEach (array, cb) {
+  return array.reduce((promise, data) => promise.then(() => cb(data)), Promise.resolve())
 }
 
-async function writeNumber(strNumber) {
-	let isNegative = false;
-	const digits = strNumber.split('');
-	await serialForEach(digits, async (digit) => {
-		if (digit === '-') {
-			isNegative = true;
-		} else {
-			await buttonClick(digit);
-		}
-	});
-	if (isNegative) {
-		await buttonClick('+-');
-	}
+async function writeNumber (strNumber) {
+  let isNegative = false
+  const digits = strNumber.split('')
+  await serialForEach(digits, async (digit) => {
+    if (digit === '-') {
+      isNegative = true
+    } else {
+      await buttonClick(digit)
+    }
+  })
+  if (isNegative) {
+    await buttonClick('+-')
+  }
 }
 
-async function keyPress(keyId) {
-	await page.keyboard.press(keyId, { force: true });
+async function keyPress (keyId) {
+  await page.keyboard.press(keyId, { force: true })
 }
 
 Given('a user opens the app', async () => {
-	await page.goto(url);
-});
+  await page.goto(url)
+})
 
 Then('the display should show the following value: {string}', async (string) => {
-	const display = await page.locator('data-testid=display').innerText();
-	// const display = await page.locator('data-testid=display').inputValue();
-	expect(display).toBe(string);
-});
+  const display = await page.locator('data-testid=display').innerText()
+  // const display = await page.locator('data-testid=display').inputValue();
+  expect(display).toBe(string)
+})
 
 Given('the user writes the following value: {string}', async (string) => {
-	await buttonClick('C');
-	await writeNumber(string);
-});
+  await buttonClick('C')
+  await writeNumber(string)
+})
 
 When('the user presses the {string} button', async (string) => {
-	await buttonClick(string);
-});
+  await buttonClick(string)
+})
 
 When('the user presses the {string} key', async (string) => {
-	await keyPress(string);
-});
+  await keyPress(string)
+})
 
 When('the user writes the number: {string}', async (string) => {
-	await writeNumber(string);
-});
+  await writeNumber(string)
+})
 
 Then('the {string} button should be disabled', async (string) => {
-	const locator = page.locator(`[data-testid="${string}"]`);
-	await expect(locator).toBeDisabled();
-});
+  const locator = page.locator(`[data-testid="${string}"]`)
+  await expect(locator).toBeDisabled()
+})
 
 Then('the {string} button should be enabled', async (string) => {
-	const locator = page.locator(`[data-testid="${string}"]`);
-	await expect(locator).toBeEnabled();
-});
+  const locator = page.locator(`[data-testid="${string}"]`)
+  await expect(locator).toBeEnabled()
+})
