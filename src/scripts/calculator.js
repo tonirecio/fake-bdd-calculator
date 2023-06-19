11e0536143507597296c12d21e23073583c9aebe
BlackSnake10 <120016418+Julio-1003@users.noreply.github.com>
2023-06-19 12:51:30 +0200

    Scenario Outline: Pressing non-operators screen buttons
    Given the user writes the following value: "<displayNumber>"
    When the user presses the "<button>" button
    Then the display should show the following value: "<displayResult>"

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,19 +1,47 @@
 const MAX_DIGITS_IN_DISPLAY = 10
 
-const setDisplay = (value) => {
+/*const setDisplay = (value) => {
   display.innerHTML = value
+}*/
+let currentValue = '0';
+
+const setDisplay = (value) => {
+  if (value === ',' && currentValue.includes(',')) {
+    return;
+  }
+  if (currentValue === '0' && value !== ',') {
+    currentValue = value;
+  } else if (currentValue.length < MAX_DIGITS_IN_DISPLAY) {
+    currentValue += value;
+  }
+  display.innerHTML = currentValue;
 }
 
-const sayHello = () => {
-  window.alert('Hello. The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.')
-}
+const resetButton = document.querySelector('button[name="clean"]');
+resetButton.addEventListener('click', () => {
+  currentValue = '0';
+  setDisplay(currentValue);
+});
 
-const reset = () => {
-  setDisplay(0)
-}
+const negate = document.querySelector('button[name="negate"]');
+negate.addEventListener('click', () => {
+  currentValue = currentValue * -1;
+  setDisplay(currentValue);
+})
 
 const display = document.querySelector('div[name="display"] span')
 document.getElementsByName('multiply')[0].addEventListener('click', () => {
   sayHello()
 })
-reset()
+
+const buttons = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'point'];
+buttons.forEach((buttonName) => {
+  const button = document.getElementsByName(buttonName)[0];
+  button.addEventListener('click', () => {
+    setDisplay(button.innerHTML);
+  });
+});
+
+const sayHello = () => {
+  window.alert('Hello. The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.')
+}
