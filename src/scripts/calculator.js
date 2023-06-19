be1ddcd82f99f729009eb2fe4448942ba8671817
Antoniogordilloe <aegordillo2@gmail.com>
2023-06-19 12:45:07 +0200

    Pressing non-operators screen buttons (done)
    14 scenarios (14 passed)
    56 steps (56 passed)

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,39 +1,112 @@
 const MAX_DIGITS_IN_DISPLAY = 10
 
+const display = document.querySelector('div[name="display"] span')
+
+let point = false
+let negated = false
+
 const setDisplay = (value) => {
-  display.innerHTML = '0'
+
+  if (display.innerHTML === '0' && value !== ',') {
+    display.innerHTML = value;
+
+  } else if (value === ',' && !point) {
+    display.innerHTML = display.innerHTML + value;
+    point = true;
+
+  } else if (value !== ',' && value !== '-') {
+    display.innerHTML = display.innerHTML + value;
+
+  }
 }
 
 const sayHello = () => {
   window.alert('Hello. The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.')
 }
 
-const addZero = () => {
-  display.innerHTML = document.getElementById("display") + '0'
-}
-
-const addOne = () => {
-  display.innerHTML = document.getElementById("display") + '1'
-}
-
 
 
 const reset = () => {
-  setDisplay(0)
+  display.innerHTML = 0
+  point = false
+  negated = false
 }
 
-const display = document.querySelector('div[name="display"] span')
+const negate = () => {
+
+  if (negated == false && display.innerHTML != '0') {
+    display.innerHTML = '-' + display.innerHTML
+    negated = true
+  }
+  else if (negated == true) {
+
+    display.innerHTML = display.innerHTML.slice(1)
+    negated = false
+
+  }
+
+}
 
 
 
-//EVENT LISTENERS
-document.getElementsByName('zero')[0].addEventListener('click', () => {
-  addZero()
+
+//EVENT LISTENERS BUTTONS
+
+document.getElementsByName('clean')[0].addEventListener('click', () => {
+  reset()
 })
 
 document.getElementsByName('zero')[0].addEventListener('click', () => {
-  addOne()
+  setDisplay('0')
 })
+
+document.getElementsByName('one')[0].addEventListener('click', () => {
+  setDisplay('1')
+})
+
+document.getElementsByName('two')[0].addEventListener('click', () => {
+  setDisplay('2')
+})
+
+document.getElementsByName('three')[0].addEventListener('click', () => {
+  setDisplay('3')
+})
+
+document.getElementsByName('four')[0].addEventListener('click', () => {
+  setDisplay('4')
+})
+
+document.getElementsByName('five')[0].addEventListener('click', () => {
+  setDisplay('5')
+})
+
+document.getElementsByName('six')[0].addEventListener('click', () => {
+  setDisplay('6')
+})
+
+document.getElementsByName('seven')[0].addEventListener('click', () => {
+  setDisplay('7')
+})
+
+document.getElementsByName('eight')[0].addEventListener('click', () => {
+  setDisplay('8')
+})
+
+document.getElementsByName('nine')[0].addEventListener('click', () => {
+  setDisplay('9')
+})
+
+document.getElementsByName('point')[0].addEventListener('click', () => {
+  setDisplay(',')
+})
+
+document.getElementsByName('negate')[0].addEventListener('click', () => {
+  negate()
+})
+
+
+
+
 
 
 document.getElementsByName('multiply')[0].addEventListener('click', () => {
