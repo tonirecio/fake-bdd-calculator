73ea0c6e3455d4e82777206eb0f6eb540e90514b
plum-bee <rafakanex1@gmail.com>
2023-06-19 13:21:40 +0200

    Scenario Outline: Pressing non-operators screen buttons

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,19 +1,58 @@
 const MAX_DIGITS_IN_DISPLAY = 10
+const DEFAULT_DISPLAY = '0'
 
 const setDisplay = (value) => {
   display.innerHTML = value
 }
 
-const sayHello = () => {
-  window.alert('Hello. The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.')
-}
-
 const reset = () => {
-  setDisplay(0)
+  setDisplay(DEFAULT_DISPLAY)
 }
 
 const display = document.querySelector('div[name="display"] span')
-document.getElementsByName('multiply')[0].addEventListener('click', () => {
-  sayHello()
+
+const numbers = [
+  'zero',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine'
+]
+let storedNumber = ''
+
+numbers.forEach((number) => {
+  const value = document.getElementsByName(number)[0]
+  value.addEventListener('click', () => {
+    if (storedNumber === '0') {
+      storedNumber = ''
+    }
+    if (storedNumber.length < MAX_DIGITS_IN_DISPLAY) {
+      storedNumber += value.innerHTML
+    }
+    setDisplay(storedNumber)
+  })
 })
+
+document.getElementsByName('point')[0].addEventListener('click', () => {
+  if (!storedNumber.includes(',')) {
+    storedNumber += ','
+  }
+  setDisplay(storedNumber)
+})
+
+document.getElementsByName('negate')[0].addEventListener('click', () => {
+  storedNumber *= -1
+  setDisplay(storedNumber)
+})
+
+document.getElementsByName('clean')[0].addEventListener('click', () => {
+  storedNumber = DEFAULT_DISPLAY
+  setDisplay(storedNumber)
+})
+
 reset()
--- tests/features/calculator.feature
+++ tests/features/calculator.feature
@@ -7,6 +7,7 @@ Given a user opens the app
 Scenario: Default display screen
 Then the display should show the following value: "0"
 
+@wip
 Scenario Outline: Pressing non-operators screen buttons
 Given the user writes the following value: "<displayNumber>"
 When the user presses the "<button>" button
