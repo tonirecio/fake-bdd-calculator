da8b7f6a16fbd3cc9d1655bfd0d75b9dd329c37c
RubenSJ04 <113702364+RubenSJ04@users.noreply.github.com>
2023-06-19 13:43:12 +0200

    Scenario Pressing non-operators screen buttons

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,8 +1,31 @@
 const MAX_DIGITS_IN_DISPLAY = 10
 
 const setDisplay = (value) => {
-  display.innerHTML = '0'
+  display.innerHTML = value
 }
+
+const appendNumber = (value) => {
+  if (display.innerHTML != 0){
+  display.innerHTML = display.innerHTML + value
+  } else {
+  display.innerHTML = value}
+}
+
+const setDecimal = (value) => {
+  display.innerHTML = display.innerHTML + value
+}
+
+const setNegation = () => {
+  let displayValue = display.innerHTML;
+
+  if (displayValue != "0") {
+    if (displayValue.slice(0, 1) === "-") {
+      display.innerHTML = displayValue.slice(1);
+    } else {
+      display.innerHTML = "-" + displayValue;
+    }
+  }
+};
 
 const sayHello = () => {
   window.alert('Hello. The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.')
@@ -13,7 +36,49 @@ const reset = () => {
 }
 
 const display = document.querySelector('div[name="display"] span')
-document.getElementsByName('multiply')[0].addEventListener('click', () => {
-  sayHello()
+
+
+// Buttons 
+// Number buttons
+document.getElementsByName('zero')[0].addEventListener('click', () => {
+  appendNumber(0)
+})
+document.getElementsByName('one')[0].addEventListener('click', () => {
+  appendNumber(1)
+})
+document.getElementsByName('two')[0].addEventListener('click', () => {
+  appendNumber(2)
+})
+document.getElementsByName('three')[0].addEventListener('click', () => {
+  appendNumber(3)
+})
+document.getElementsByName('four')[0].addEventListener('click', () => {
+  appendNumber(4)
+})
+document.getElementsByName('five')[0].addEventListener('click', () => {
+  appendNumber(5)
+})
+document.getElementsByName('six')[0].addEventListener('click', () => {
+  appendNumber(6)
+})
+document.getElementsByName('seven')[0].addEventListener('click', () => {
+  appendNumber(7)
+})
+document.getElementsByName('eight')[0].addEventListener('click', () => {
+  appendNumber(8)
+})
+document.getElementsByName('nine')[0].addEventListener('click', () => {
+  appendNumber(9)
+})
+
+// Other buttons
+document.getElementsByName('point')[0].addEventListener('click', () => {
+  setDecimal(',')
+})
+document.getElementsByName('clean')[0].addEventListener('click', () => {
+  reset()
+})
+document.getElementsByName('negate')[0].addEventListener('click', () => {
+  setNegation()
 })
 reset()
--- tests/features/calculator.feature
+++ tests/features/calculator.feature
@@ -2,10 +2,12 @@ Feature: Calculator
 
 Background:
 Given a user opens the app
-@wip
+
+@scn1
 Scenario: Default display screen
 Then the display should show the following value: "0"
 
+@scn2
 Scenario Outline: Pressing non-operators screen buttons
 Given the user writes the following value: "<displayNumber>"
 When the user presses the "<button>" button
@@ -28,6 +30,7 @@ Examples:
 |             1 |     +- |            -1 |
 |            -1 |     +- |             1 |
 
+@scn3
 Scenario Outline: Pressing non-operators keys
 Given the user writes the following value: "<displayNumber>"
 When the user presses the "<key>" key
