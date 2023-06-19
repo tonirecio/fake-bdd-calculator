7387a4c016e0c5ac26889196b168aaeaeac06a65
plum-bee <rafakanex1@gmail.com>
2023-06-19 14:00:22 +0200

    Scenario Outline: Pressing non-operators keys

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -46,13 +46,47 @@ document.getElementsByName('point')[0].addEventListener('click', () => {
 })
 
 document.getElementsByName('negate')[0].addEventListener('click', () => {
-  storedNumber *= -1
+  storedNumber = (parseFloat(storedNumber) * -1).toString()
   setDisplay(storedNumber)
 })
 
 document.getElementsByName('clean')[0].addEventListener('click', () => {
   storedNumber = DEFAULT_DISPLAY
   setDisplay(storedNumber)
+})
+
+document.addEventListener('keydown', (event) => {
+  const key = event.key
+  if (key >= '0' && key <= '9') {
+    const numberIndex = parseInt(key, 10)
+    const numberName = numbers[numberIndex]
+    const value = document.getElementsByName(numberName)[0]
+
+    if (storedNumber === '0') {
+      storedNumber = ''
+    }
+    if (storedNumber.length < MAX_DIGITS_IN_DISPLAY) {
+      storedNumber += value.innerHTML
+    }
+    setDisplay(storedNumber)
+  }
+
+  if (key === 'Control') {
+    storedNumber = (parseFloat(storedNumber) * -1).toString()
+    setDisplay(storedNumber)
+  }
+
+  if (key === 'Escape') {
+    storedNumber = DEFAULT_DISPLAY
+    setDisplay(storedNumber)
+  }
+
+  if (key === ',') {
+    if (!storedNumber.includes(',')) {
+      storedNumber += ','
+    }
+    setDisplay(storedNumber)
+  }
 })
 
 reset()
