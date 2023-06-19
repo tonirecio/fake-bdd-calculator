5446239c4f874e58bdcde32d3adb91aa4657e596
JGedff <jordimarmi1@gmail.com>
2023-06-19 12:53:29 +0200

    Scenario: Default display screen

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -17,3 +17,84 @@ document.getElementsByName('multiply')[0].addEventListener('click', () => {
   sayHello()
 })
 reset()
+
+const addNumber = (num) => {
+  const actualNumber = display.innerHTML
+
+  if (Number(actualNumber) === 0) {
+    setDisplay(num)
+  } else {
+    const newNumber = actualNumber + num
+
+    setDisplay(newNumber)
+  }
+}
+
+const addPoint = () => {
+  const actualNumber = display.innerHTML
+
+  if (Number(actualNumber) % 1 === 0 || Number(actualNumber) === 0) {
+    const newDisplay = actualNumber + ','
+
+    setDisplay(newDisplay)
+  }
+}
+
+const negateNumber = () => {
+  const negated = Number(display.innerHTML) * -1
+
+  setDisplay(negated)
+}
+
+/* Listeners for buttons */
+document.getElementsByName('one')[0].addEventListener('click', () => {
+  addNumber(1)
+})
+
+document.getElementsByName('two')[0].addEventListener('click', () => {
+  addNumber(2)
+})
+
+document.getElementsByName('three')[0].addEventListener('click', () => {
+  addNumber(3)
+})
+
+document.getElementsByName('four')[0].addEventListener('click', () => {
+  addNumber(4)
+})
+
+document.getElementsByName('five')[0].addEventListener('click', () => {
+  addNumber(5)
+})
+
+document.getElementsByName('six')[0].addEventListener('click', () => {
+  addNumber(6)
+})
+
+document.getElementsByName('seven')[0].addEventListener('click', () => {
+  addNumber(7)
+})
+
+document.getElementsByName('eight')[0].addEventListener('click', () => {
+  addNumber(8)
+})
+
+document.getElementsByName('nine')[0].addEventListener('click', () => {
+  addNumber(9)
+})
+
+document.getElementsByName('zero')[0].addEventListener('click', () => {
+  addNumber(0)
+})
+
+document.getElementsByName('point')[0].addEventListener('click', () => {
+  addPoint()
+})
+
+document.getElementsByName('negate')[0].addEventListener('click', () => {
+  negateNumber()
+})
+
+document.getElementsByName('clean')[0].addEventListener('click', () => {
+  setDisplay(0)
+})
