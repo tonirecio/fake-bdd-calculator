5c062dd02284dad266b08e2826d4c08742a938b5
Christian Cuesta <christianluminor@gmail.com>
2023-06-19 12:21:55 +0200

    Scenario Outline: Pressing non-operators screen buttons

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -16,16 +16,79 @@ const display = document.querySelector('div[name="display"] span')
 
 // Buttons
 
+const appendNumber = (value) => {
+  if (display.innerHTML != 0){
+  display.innerHTML = display.innerHTML + value
+  } else {
+  display.innerHTML = value}
+}
+
+const appendSymbol = (value) => {
+  display.innerHTML = display.innerHTML + value
+}
+
+const setNegation = () => {
+  let displayValue = display.innerHTML;
+
+  if (displayValue !== "0") {
+    if (displayValue.slice(0, 1) === "-") {
+      display.innerHTML = displayValue.slice(1);
+    } else {
+      display.innerHTML = "-" + displayValue;
+    }
+  }
+};
+
+
 document.getElementsByName('zero')[0].addEventListener('click', () => {
-  setDisplay(0)
+  appendNumber(0)
 })
 
 document.getElementsByName('one')[0].addEventListener('click', () => {
-  setDisplay(1)
+  appendNumber(1)
 })
 
-document.getElementsByName('multiply')[0].addEventListener('click', () => {
-  setDisplay('x')
+document.getElementsByName('two')[0].addEventListener('click', () => {
+  appendNumber(2)
 })
 
-reset()
+document.getElementsByName('three')[0].addEventListener('click', () => {
+  appendNumber(3)
+})
+
+document.getElementsByName('four')[0].addEventListener('click', () => {
+  appendNumber(4)
+})
+
+document.getElementsByName('five')[0].addEventListener('click', () => {
+  appendNumber(5)
+})
+
+document.getElementsByName('six')[0].addEventListener('click', () => {
+  appendNumber(6)
+})
+
+document.getElementsByName('seven')[0].addEventListener('click', () => {
+  appendNumber(7)
+})
+
+document.getElementsByName('eight')[0].addEventListener('click', () => {
+  appendNumber(8)
+})
+
+document.getElementsByName('nine')[0].addEventListener('click', () => {
+  appendNumber(9)
+})
+
+document.getElementsByName('point')[0].addEventListener('click', () => {
+  appendSymbol(',')
+})
+
+document.getElementsByName('negate')[0].addEventListener('click', () => {
+  setNegation()
+})
+
+document.getElementsByName('clean')[0].addEventListener('click', () => {
+  reset()
+})
+
