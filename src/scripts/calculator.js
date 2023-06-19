4ffc53b5e99f6555b33921d573e6fb05ce6ac7e5
Anas Lharrak <anlhbiada@gmail.com>
2023-06-19 12:38:28 +0200

    Scenario Outline: Pressing non-operators screen buttons

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,19 +1,71 @@
 const MAX_DIGITS_IN_DISPLAY = 10
+let valueDisplay = "";
+
+const display = document.querySelector('div[name="display"] span')
+
 
 const setDisplay = (value) => {
-  display.innerHTML = value
+  
+  if(valueDisplay.length > MAX_DIGITS_IN_DISPLAY){
+    maxNumberWarning();
+  }
+  else if(valueDisplay === "0" && value != ","){
+    valueDisplay = value;
+  }else{
+    valueDisplay = valueDisplay + value;
+  }
+    display.innerHTML = valueDisplay
 }
 
-const sayHello = () => {
-  window.alert('Hello. The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.')
+const maxNumberWarning = () => {
+  window.alert('The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.')
 }
 
 const reset = () => {
-  setDisplay(0)
+  setDisplay("0")
 }
 
-const display = document.querySelector('div[name="display"] span')
-document.getElementsByName('multiply')[0].addEventListener('click', () => {
-  sayHello()
+document.getElementsByName('seven')[0].addEventListener('click', () => {
+  setDisplay("7")
 })
+document.getElementsByName('eight')[0].addEventListener('click', () => {
+  setDisplay("8")
+})
+document.getElementsByName('nine')[0].addEventListener('click', () => {
+  setDisplay("9")
+})
+document.getElementsByName('four')[0].addEventListener('click', () => {
+  setDisplay("4")
+})
+document.getElementsByName('five')[0].addEventListener('click', () => {
+  setDisplay("5")
+})
+document.getElementsByName('six')[0].addEventListener('click', () => {
+  setDisplay("6")
+})
+document.getElementsByName('one')[0].addEventListener('click', () => {
+  setDisplay("1")
+})
+document.getElementsByName('two')[0].addEventListener('click', () => {
+  setDisplay("2")
+})
+document.getElementsByName('three')[0].addEventListener('click', () => {
+  setDisplay("3")
+})
+document.getElementsByName('zero')[0].addEventListener('click', () => {
+  setDisplay("0")
+})
+document.getElementsByName('clean')[0].addEventListener('click', () => {
+  valueDisplay = "0";
+  setDisplay(valueDisplay)
+})
+document.getElementsByName('point')[0].addEventListener('click', () => {
+  setDisplay(",")
+})
+document.getElementsByName('negate')[0].addEventListener('click', () => {
+  valueDisplay = valueDisplay * -1
+  display.innerHTML = valueDisplay;
+})
+
+
 reset()
