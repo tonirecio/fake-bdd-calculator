a87f28883f8062278326b4fc9e8c6fdea0187eda
gdg2004396 <113702199+gdg2004396@users.noreply.github.com>
2023-06-19 11:23:23 +0200

    Test Value 0

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,7 +1,7 @@
 const MAX_DIGITS_IN_DISPLAY = 10
 
 const setDisplay = (value) => {
-  display.innerHTML = 'INTERNS'
+  display.innerHTML = value
 }
 
 const sayHello = () => {
@@ -14,6 +14,6 @@ const reset = () => {
 
 const display = document.querySelector('div[name="display"] span')
 document.getElementsByName('multiply')[0].addEventListener('click', () => {
-  sayHello()
+  setDisplay("X")
 })
 reset()
