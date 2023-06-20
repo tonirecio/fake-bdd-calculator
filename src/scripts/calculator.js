721dd22d4fff80812b5535d4ffc89c36955af6b3
gdg2004396 <113702199+gdg2004396@users.noreply.github.com>
2023-06-20 10:17:02 +0200

    Scenario Outline: Pressing non-operators keys

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -30,7 +30,7 @@ const DisplayAcu = (value) => {
       display.innerHTML = newValue;
     } else {
       window.alert('Hello. The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.');
-      reset();
+      // reset();
     }
   }
 };
@@ -111,4 +111,74 @@ document.getElementsByName('eight')[0].addEventListener('click', () => {
 
 document.getElementsByName('nine')[0].addEventListener('click', () => {
   DisplayAcu("9");
+});
+
+document.addEventListener('keydown', (event) => {
+  const targetElement = document.activeElement;
+  const targetTagName = targetElement.tagName.toLowerCase();
+  if (targetTagName === 'input' || targetTagName === 'textarea') {
+    return;
+  }
+
+  const key = event.key;
+
+  switch (key) {
+    case '+':
+      DisplayAcu("+");
+      break;
+    case '-':
+      DisplayAcu("-");
+      break;
+    case '*':
+      DisplayAcu("x");
+      break;
+    case '/':
+      DisplayAcu("÷");
+      break;
+    case '.':
+    case ',':
+      if (!hasDecimal) {
+        DisplayAcu(",");
+      }
+      break;
+    case '0':
+      DisplayAcu("0");
+      break;
+    case '1':
+      DisplayAcu("1");
+      break;
+    case '2':
+      DisplayAcu("2");
+      break;
+    case '3':
+      DisplayAcu("3");
+      break;
+    case '4':
+      DisplayAcu("4");
+      break;
+    case '5':
+      DisplayAcu("5");
+      break;
+    case '6':
+      DisplayAcu("6");
+      break;
+    case '7':
+      DisplayAcu("7");
+      break;
+    case '8':
+      DisplayAcu("8");
+      break;
+    case '9':
+      DisplayAcu("9");
+      break;
+    case 'Control':
+      const negateButton = document.getElementsByName('negate')[0];
+      negateButton.click();
+      break;
+    case 'Escape':
+      reset();
+      break;
+    default:
+      break;
+  }
 });
