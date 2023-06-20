6002903c8dcb40d4b075c6606dde21b96869dda0
cristinaco32 <cristinacosiallsibiza@gmail.com>
2023-06-20 11:25:16 +0200

    scenario pressing non-operators screen buttons test passed

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Scenario: The display show the value: "0" 
 Comentario: El valor 0 tiene la posiblidad de ser un string.  
+
+Scenario Outline: Pressing non-operator screen buttons.
--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,15 +1,22 @@
 const MAX_DIGITS_IN_DISPLAY = 10
 
 const setDisplay = (value) => {
-  display.innerHTML = 0
+  display.innerHTML = value
 }
 
 const sayHello = () => {
   window.alert('Hello. The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.')
 }
 
+const updateDisplay = (value) => {
+  setDisplay(value)
+}
+
+let currentValue
+
 const reset = () => {
-  setDisplay(0)
+  currentValue = ''
+  updateDisplay(0)
 }
 
 const display = document.querySelector('div[name="display"] span')
@@ -17,3 +24,55 @@ document.getElementsByName('multiply')[0].addEventListener('click', () => {
   sayHello()
 })
 reset()
+
+//const charButtons = document.querySelectorAll('[aria-label]')
+
+const numberButtons = document.querySelectorAll('button:not([aria-label])')
+numberButtons.forEach(numberButton =>{
+  numberButton.addEventListener('click', () => {
+    setDisplay(numberButton.textContent)
+  })
+})
+
+document.getElementsByName('clean')[0].addEventListener('click', () => {
+  reset()
+})
+/*
+numberButtons.forEach(numberButton => {
+  numberButton.addEventListener('click', () =>{
+  if(parseInt(currentValue) === 0){
+    currentValue = numberButton.textContent
+  }
+  else {
+    currentValue += numberButton.textContent
+  }
+  setDisplay(currentValue)
+  })
+})
+*/
+
+numberButtons.forEach(numberButton => {
+  numberButton.addEventListener('click', () =>{
+  if(currentValue != 0  || numberButton.textContent == ',' ){
+    currentValue += numberButton.textContent
+  }
+  else {
+    currentValue = numberButton.textContent
+  }
+  setDisplay(currentValue)
+  })
+})
+
+const negateButton = (displayValues) => {
+  displayValues = displayValues * -1
+  updateDisplay(displayValues)
+}
+
+document.getElementsByName('negate')[0].addEventListener('click', () => {
+  negateButton(display.innerHTML)
+})
+/*
+document.getElementsByName('point')[0].addEventListener('click', () => {
+  displayValues()
+})
+*/
