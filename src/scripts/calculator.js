0f8a5e43cf29bc51000b2355315254d6783702c0
elna <elna.ballesta.7e6@itb.cat>
2023-06-20 10:58:31 +0200

    Scenario Outline: Writing numbers (done)

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,5 +1,10 @@
 const MAX_DIGITS_IN_DISPLAY = 10
 
+const getDisplay = () => {
+  const num = display.innerHTML
+  return (num.replace(',', '.'))
+}
+
 const setDisplay = (value) => {
   display.innerHTML = value
 }
@@ -13,37 +18,58 @@ const reset = () => {
 }
 
 const addNum = (value) => {
-  if (!isNaN(value)) {
-    if (display.innerHTML === '0') {
-      setDisplay(value)
-    } else {
-      setDisplay(display.innerHTML.concat(value))
-    }
+  if (display.innerHTML === '0') {
+    setDisplay(value)
   } else {
-    if (value === 'C') {
-      reset()
-    } else if (value === ',') {
-      setDisplay(display.innerHTML.concat(value))
-    } else if (value === 'negate') {
-      setDisplay(parseInt(display.innerHTML) * -1)
-    }
+    setDisplay(display.innerHTML.concat(value))
   }
 }
 
-const display = document.querySelector('div[name="display"] span')
+const negateNum = () => {
+  let num = display.innerHTML
+  if (Number(getDisplay()) !== 0) {
+    if (num.startsWith('-')) {
+      num = num.slice(1)
+    } else {
+      num = '-' + num
+    }
+    setDisplay(num)
+  }
+}
+
+const addPoint = () => {
+  const num = display.innerHTML
+  if (!num.includes(',')) {
+    setDisplay(num.concat(','))
+  }
+}
+
+// Buttons events
 
 const buttons = document.getElementsByName('keypad')[0].getElementsByTagName('button')
 const listeners = (buttons) => {
   for (const button of buttons) {
     button.addEventListener('click', () => {
-      addNum(button.innerHTML)
+      if (!isNaN(button.innerHTML)) {
+        addNum(button.innerHTML)
+      } else {
+        if (button.innerHTML === 'C') {
+          reset()
+        } else if (button.innerHTML === ',') {
+          addPoint()
+        } else if (button.innerHTML === 'negate') {
+          negateNum()
+        }
+      }
     })
   }
 }
 
 document.getElementsByName('negate')[0].addEventListener('click', () => {
-  addNum('negate')
+  negateNum()
 })
+
+// Keys events
 
 document.addEventListener('keyup', (key) => {
   console.log(key.key)
@@ -51,19 +77,20 @@ document.addEventListener('keyup', (key) => {
     addNum(key.key)
   } else {
     if (key.key === 'Control') {
-      addNum('negate')
+      negateNum()
     } else if (key.key === 'Escape') {
-      addNum('C')
-    } else if (key.key === ",") {
-      addNum(",")
+      reset()
+    } else if (key.key === ',') {
+      addPoint()
     }
   }
 })
 
-
-
 document.getElementsByName('multiply')[0].addEventListener('click', () => {
   sayHello()
 })
+
+const display = document.querySelector('div[name="display"] span')
+
 reset()
 listeners(buttons)
