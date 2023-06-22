763b1136a8b5725e0066bf44b3cc751c19e8a59c
m-gusta-666 <martigustamante@gmail.com>
2023-06-22 12:06:55 +0200

    Scenario: divide by zero. Refactored code

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -5,6 +5,7 @@ let currentOperation = null
 let previousOperand = 0
 let currentOperand = null
 let overrideDisplay = false
+let displayedText = '0'
 
 const init = () => {
   addEventsToButtons()
@@ -16,21 +17,22 @@ const reset = () => {
   previousOperand = 0
   currentOperand = null
   currentOperation = null
-  setDisplay(0)
+  displayedText = '0'
+  setDisplay()
 }
 
 const addEventsToButtons = () => {
   // Number buttons
-  document.querySelector('button[name="zero"]').addEventListener('click', () => writeNumber(0))
-  document.querySelector('button[name="one"]').addEventListener('click', () => writeNumber(1))
-  document.querySelector('button[name="two"]').addEventListener('click', () => writeNumber(2))
-  document.querySelector('button[name="three"]').addEventListener('click', () => writeNumber(3))
-  document.querySelector('button[name="four"]').addEventListener('click', () => writeNumber(4))
-  document.querySelector('button[name="five"]').addEventListener('click', () => writeNumber(5))
-  document.querySelector('button[name="six"]').addEventListener('click', () => writeNumber(6))
-  document.querySelector('button[name="seven"]').addEventListener('click', () => writeNumber(7))
-  document.querySelector('button[name="eight"]').addEventListener('click', () => writeNumber(8))
-  document.querySelector('button[name="nine"]').addEventListener('click', () => writeNumber(9))
+  document.querySelector('button[name="zero"]').addEventListener('click', () => writeNumber('0'))
+  document.querySelector('button[name="one"]').addEventListener('click', () => writeNumber('1'))
+  document.querySelector('button[name="two"]').addEventListener('click', () => writeNumber('2'))
+  document.querySelector('button[name="three"]').addEventListener('click', () => writeNumber('3'))
+  document.querySelector('button[name="four"]').addEventListener('click', () => writeNumber('4'))
+  document.querySelector('button[name="five"]').addEventListener('click', () => writeNumber('5'))
+  document.querySelector('button[name="six"]').addEventListener('click', () => writeNumber('6'))
+  document.querySelector('button[name="seven"]').addEventListener('click', () => writeNumber('7'))
+  document.querySelector('button[name="eight"]').addEventListener('click', () => writeNumber('8'))
+  document.querySelector('button[name="nine"]').addEventListener('click', () => writeNumber('9'))
   // Non operator buttons
   document.querySelector('button[name="point"]').addEventListener('click', writeComma)
   document.querySelector('button[name="clean"]').addEventListener('click', reset)
@@ -61,32 +63,30 @@ const addEventsToKeyboard = () => {
 }
 
 const displayedNumber = () => {
-  const formattedDisplay = display.innerHTML.replace(',', '.')
+  const formattedDisplay = displayedText.replace(',', '.')
   return Number(formattedDisplay)
 }
 
-const displayedText = () => {
-  return display.innerHTML
-}
-
-const setDisplay = (value) => {
-  value = value.toString().replace('.', ',')
-  display.innerHTML = value
+const setDisplay = () => {
+  display.innerHTML = displayedText
 }
 
 const negateDisplay = () => {
   if (displayedNumber() !== 0) {
     if (displayedNumber() < 0) {
-      setDisplay(displayedText().substring(1))
+      displayedText = displayedText.substring(1)
+      setDisplay()
     } else {
-      setDisplay('-' + displayedText())
+      displayedText = '-' + displayedText
+      setDisplay()
     }
   }
 }
 
 const appendDisplay = (content) => {
   if (getDigitNumber(displayedNumber()) < MAX_DIGITS_IN_DISPLAY) {
-    setDisplay(displayedText() + content.toString())
+    displayedText += content.toString()
+    setDisplay()
   }
 }
 
@@ -95,16 +95,17 @@ const getDigitNumber = (number) => {
 }
 
 const writeNumber = (number) => {
-  if (displayedText() === '0' || overrideDisplay) {
+  if (displayedText === '0' || overrideDisplay) {
     overrideDisplay = false
-    setDisplay(number)
+    displayedText = number
+    setDisplay()
   } else {
     appendDisplay(number)
   }
 }
 
 const writeComma = () => {
-  if (!displayedText().includes(COMMA_SYMBOL)) { appendDisplay(COMMA_SYMBOL) }
+  if (!displayedText.includes(COMMA_SYMBOL)) { appendDisplay(COMMA_SYMBOL) }
 }
 
 const setOperation = (operation) => {
@@ -138,21 +139,22 @@ const performOperation = () => {
   } else {
     result = displayedNumber()
   }
-  result = formatResult(result)
-  setDisplay(result)
+  displayedText = formatResult(result)
+  setDisplay()
   overrideDisplay = true
   currentOperation = null
 }
 
 const formatResult = (result) => {
-  if (Math.abs(result) >= Math.pow(10, MAX_DIGITS_IN_DISPLAY)) result = 'ERROR'
+  if (Math.abs(result) >= Math.pow(10, MAX_DIGITS_IN_DISPLAY) || isNaN(result)) result = 'ERROR'
   if (result !== 'ERROR') {
     if (getDigitNumber(result) > MAX_DIGITS_IN_DISPLAY) {
       const integerPartDigits = parseInt(result.toString()).toString().length
-      result = 1 * result.toFixed(MAX_DIGITS_IN_DISPLAY - integerPartDigits)
+      // Round by using toFixed() and remove trailing zeros by using parseFloat()
+      result = parseFloat(result.toFixed(MAX_DIGITS_IN_DISPLAY - integerPartDigits))
     }
   }
-  return result
+  return result.toString().replace('.', COMMA_SYMBOL)
 }
 // TODO: organitzar codi
 
