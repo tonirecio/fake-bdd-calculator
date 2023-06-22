c435660e62d5e78f88b3da1920896ec74ccc8ccc
m-gusta-666 <martigustamante@gmail.com>
2023-06-22 10:58:55 +0200

    Scenario: previous result easier Refactored code

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,17 +1,15 @@
 const MAX_DIGITS_IN_DISPLAY = 10
+const COMMA_SYMBOL = ','
+const display = document.querySelector('div[name="display"] span')
 let currentOperation = null
 let previousOperand = 0
 let currentOperand = null
 let overrideDisplay = false
 
-const displayedNumber = () => {
-  const formattedDisplay = display.innerHTML.replace(',', '.')
-  return Number(formattedDisplay)
-}
-
-const setDisplay = (value) => {
-  value = value.toString().replace('.', ',')
-  display.innerHTML = value
+const init = () => {
+  addEventsToButtons()
+  addEventsToKeyboard()
+  reset()
 }
 
 const reset = () => {
@@ -21,19 +19,74 @@ const reset = () => {
   setDisplay(0)
 }
 
-const negate = () => {
-  if (display.innerHTML !== '0' && display.innerHTML !== '0,') {
-    if (display.innerHTML[0] === '-') {
-      display.innerHTML = display.innerHTML.substring(1)
+const addEventsToButtons = () => {
+  // Number buttons
+  document.querySelector('button[name="zero"]').addEventListener('click', () => writeNumber(0))
+  document.querySelector('button[name="one"]').addEventListener('click', () => writeNumber(1))
+  document.querySelector('button[name="two"]').addEventListener('click', () => writeNumber(2))
+  document.querySelector('button[name="three"]').addEventListener('click', () => writeNumber(3))
+  document.querySelector('button[name="four"]').addEventListener('click', () => writeNumber(4))
+  document.querySelector('button[name="five"]').addEventListener('click', () => writeNumber(5))
+  document.querySelector('button[name="six"]').addEventListener('click', () => writeNumber(6))
+  document.querySelector('button[name="seven"]').addEventListener('click', () => writeNumber(7))
+  document.querySelector('button[name="eight"]').addEventListener('click', () => writeNumber(8))
+  document.querySelector('button[name="nine"]').addEventListener('click', () => writeNumber(9))
+  // Non operator buttons
+  document.querySelector('button[name="point"]').addEventListener('click', writeComma)
+  document.querySelector('button[name="clean"]').addEventListener('click', reset)
+  document.querySelector('button[name="negate"]').addEventListener('click', negateDisplay)
+  // Operator buttons
+  document.querySelector('button[name="equal"]').addEventListener('click', performOperation)
+  document.querySelector('button[name="sum"').addEventListener('click', () => setOperation('sum'))
+  document.querySelector('button[name="subtract"').addEventListener('click', () => setOperation('subtract'))
+  document.querySelector('button[name="multiply"').addEventListener('click', () => setOperation('multiply'))
+  document.querySelector('button[name="divide"').addEventListener('click', () => setOperation('divide'))
+}
+
+const addEventsToKeyboard = () => {
+  document.addEventListener('keydown', (event) => {
+    const keyName = event.key
+    if (!isNaN(keyName)) {
+      writeNumber(parseInt(keyName))
     } else {
-      display.innerHTML = '-' + display.innerHTML
+      if (keyName === ',') {
+        writeComma()
+      } else if (keyName === 'Escape') {
+        reset()
+      } else if (keyName === 'Control') {
+        negateDisplay()
+      }
+    }
+  })
+}
+
+const displayedNumber = () => {
+  const formattedDisplay = display.innerHTML.replace(',', '.')
+  return Number(formattedDisplay)
+}
+
+const displayedText = () => {
+  return display.innerHTML
+}
+
+const setDisplay = (value) => {
+  value = value.toString().replace('.', ',')
+  display.innerHTML = value
+}
+
+const negateDisplay = () => {
+  if (displayedNumber() !== 0) {
+    if (displayedNumber() < 0) {
+      setDisplay(displayedText().substring(1))
+    } else {
+      setDisplay('-' + displayedText())
     }
   }
 }
 
-const append = (content) => {
+const appendDisplay = (content) => {
   if (getDigitNumber(displayedNumber()) < MAX_DIGITS_IN_DISPLAY) {
-    setDisplay(display.innerHTML + content.toString())
+    setDisplay(displayedText() + content.toString())
   }
 }
 
@@ -41,13 +94,29 @@ const getDigitNumber = (number) => {
   return number.toString().replace('.', '').length
 }
 
-const writeNumber = (button) => {
-  if (display.innerHTML === '0' || overrideDisplay) {
+const writeNumber = (number) => {
+  if (displayedText() === '0' || overrideDisplay) {
     overrideDisplay = false
-    setDisplay(button.innerHTML)
+    setDisplay(number)
   } else {
-    append(button.innerHTML)
+    appendDisplay(number)
   }
+}
+
+const writeComma = () => {
+  if (!displayedText().includes(COMMA_SYMBOL)) { appendDisplay(COMMA_SYMBOL) }
+}
+
+const setOperation = (operation) => {
+  if (currentOperation === null) {
+    previousOperand = displayedNumber()
+  } else if (!overrideDisplay) {
+    currentOperand = displayedNumber()
+    performOperation()
+    previousOperand = displayedNumber()
+  }
+  currentOperation = operation
+  overrideDisplay = true
 }
 
 const performOperation = () => {
@@ -72,6 +141,7 @@ const performOperation = () => {
   result = formatResult(result)
   setDisplay(result)
   overrideDisplay = true
+  currentOperation = null
 }
 
 const formatResult = (result) => {
@@ -84,66 +154,6 @@ const formatResult = (result) => {
   }
   return result
 }
+// TODO: organitzar codi
 
-const display = document.querySelector('div[name="display"] span')
-reset()
-
-// Button handling
-const buttons = document.querySelectorAll('div[name="keypad"] button')
-
-buttons.forEach(button => {
-  if (!isNaN(button.innerHTML)) {
-    button.addEventListener('click', () => {
-      writeNumber(button)
-    })
-  } else {
-    switch (button.getAttribute('name')) {
-      case 'point':
-        button.addEventListener('click', () => {
-          if (!display.innerHTML.includes(button.innerHTML)) {
-            append(button.innerHTML)
-          }
-        })
-        break
-
-      case 'clean':
-        button.addEventListener('click', reset)
-        break
-
-      case 'negate':
-        button.addEventListener('click', negate)
-        break
-
-      case 'equal':
-        button.addEventListener('click', performOperation)
-        break
-
-      default:
-        button.addEventListener('click', () => {
-          currentOperation = button.getAttribute('name')
-          previousOperand = displayedNumber()
-          overrideDisplay = true
-        })
-    }
-  }
-})
-
-// Keyboard handling
-document.addEventListener('keydown', (event) => {
-  const name = event.key
-  if (!isNaN(name)) {
-    if (display.innerHTML === '0') {
-      setDisplay(name)
-    } else {
-      append(name)
-    }
-  } else {
-    if (name === ',') {
-      if (!display.innerHTML.includes(',')) { append(',') }
-    } else if (name === 'Escape') {
-      reset()
-    } else if (name === 'Control') {
-      negate()
-    }
-  }
-})
+init()
