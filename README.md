198b3771083b181e49b2f04ade756297efb5d770
plum-bee <rafakanex1@gmail.com>
2023-06-22 11:54:12 +0200

    Refactoring and 2 operations scenearios
    Scenario Outline: Performing two number operations
    
    Scenario Outline: Before clicking the equal button

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # fake-bdd-calculator
-## Scenario 1: Default display screen
+## Scenario : Default display screen
 ### Instead of being a fixed value, it should be variable.
 
 ## Scenario Outline: Pressing non-operators screen buttons
--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,5 +1,4 @@
 const MAX_DIGITS_IN_DISPLAY = 10
-const DEFAULT_DISPLAY = '0'
 
 const setDisplay = value => {
   const updatedValue = value.replace('.', ',')
@@ -7,24 +6,80 @@ const setDisplay = value => {
 }
 
 const reset = () => {
-  storedNumber = DEFAULT_DISPLAY
+  storedNumber = '0'
   setDisplay(storedNumber)
 }
 
-const negate = value => {
-  const temp = value
-  value = (parseFloat(storedNumber) * -1).toString()
-  if (temp.endsWith('.')) {
-    value += '.'
+const negate = () => {
+  if (storedNumber !== '0') {
+    const temp = storedNumber
+    storedNumber = (parseFloat(storedNumber) * -1).toString()
+    if (temp.endsWith('.')) {
+      storedNumber += '.'
+    }
+    setDisplay(storedNumber)
   }
-  storedNumber = value
+}
+
+const appendNumber = value => {
+  if (storedNumber === '0') {
+    storedNumber = ''
+  }
+  if (storedNumber.replace('.', '').length < MAX_DIGITS_IN_DISPLAY) {
+    storedNumber += value
+  }
   setDisplay(storedNumber)
+}
+
+const appendDot = () => {
+  if (
+    !storedNumber.includes('.') &&
+    storedNumber.length < MAX_DIGITS_IN_DISPLAY
+  ) {
+    storedNumber += '.'
+  }
+  setDisplay(storedNumber)
+}
+
+const updateOperation = operationType => {
+  tempNumber = parseFloat(storedNumber).toString()
+  storedNumber = '0'
+  operation = operationType
+}
+
+const executeOperation = operationType => {
+  const firstOperand = parseFloat(tempNumber)
+  const secondOperand = parseFloat(storedNumber)
+
+  switch (operationType) {
+    case 'sum':
+      result = firstOperand + secondOperand
+      break
+    case 'subtract':
+      result = firstOperand - secondOperand
+      break
+    case 'multiply':
+      result = firstOperand * secondOperand
+      break
+    case 'divide':
+      result = firstOperand / secondOperand
+      break
+    default:
+      break
+  }
+
+  result = parseFloat(result.toPrecision(MAX_DIGITS_IN_DISPLAY))
+  setDisplay(result.toString())
 }
 
 const display = document.querySelector('div[name="display"] span')
 
-const numbers = [
-  'zero',
+let storedNumber = '0'
+let tempNumber = '0'
+let operation = ''
+let result = 0
+
+const buttons = [
   'one',
   'two',
   'three',
@@ -33,71 +88,141 @@ const numbers = [
   'six',
   'seven',
   'eight',
-  'nine'
+  'nine',
+  'zero',
+  'point',
+  'clean',
+  'negate',
+  'sum',
+  'subtract',
+  'multiply',
+  'divide',
+  'equal'
 ]
-let storedNumber = DEFAULT_DISPLAY
 
-numbers.forEach(number => {
-  const value = document.getElementsByName(number)[0]
+buttons.forEach(button => {
+  const value = document.getElementsByName(button)[0]
   value.addEventListener('click', () => {
-    if (storedNumber === '0') {
-      storedNumber = ''
+    switch (button) {
+      case 'one':
+        appendNumber(1)
+        break
+      case 'two':
+        appendNumber(2)
+        break
+      case 'three':
+        appendNumber(3)
+        break
+      case 'four':
+        appendNumber(4)
+        break
+      case 'five':
+        appendNumber(5)
+        break
+      case 'six':
+        appendNumber(6)
+        break
+      case 'seven':
+        appendNumber(7)
+        break
+      case 'eight':
+        appendNumber(8)
+        break
+      case 'nine':
+        appendNumber(9)
+        break
+      case 'zero':
+        appendNumber(0)
+        break
+      case 'point':
+        appendDot()
+        break
+      case 'clean':
+        reset()
+        break
+      case 'negate':
+        negate()
+        break
+      case 'sum':
+        updateOperation('sum')
+        break
+      case 'subtract':
+        updateOperation('subtract')
+        break
+      case 'multiply':
+        updateOperation('multiply')
+        break
+      case 'divide':
+        updateOperation('divide')
+        break
+      case 'equal':
+        executeOperation(operation)
+        break
+      default:
+        break
     }
-    if (storedNumber.replace('.', '').length < MAX_DIGITS_IN_DISPLAY) {
-      storedNumber += value.innerHTML
-    }
-    setDisplay(storedNumber)
   })
-})
-
-document.getElementsByName('point')[0].addEventListener('click', () => {
-  if (
-    !storedNumber.includes('.') &&
-    storedNumber.length < MAX_DIGITS_IN_DISPLAY
-  ) {
-    storedNumber += '.'
-  }
-  setDisplay(storedNumber)
-})
-
-document.getElementsByName('negate')[0].addEventListener('click', () => {
-  negate(storedNumber)
-})
-
-document.getElementsByName('clean')[0].addEventListener('click', () => {
-  reset()
 })
 
 document.addEventListener('keydown', event => {
   const key = event.key
-  if (key >= '0' && key <= '9') {
-    const value = parseInt(key, 10)
-
-    if (storedNumber === '0') {
-      storedNumber = ''
-    }
-    if (storedNumber.replace('.', '').length < MAX_DIGITS_IN_DISPLAY) {
-      storedNumber += value
-    }
-    setDisplay(storedNumber)
-  }
-
-  if (key === 'Control') {
-    negate(storedNumber)
-  }
-
-  if (key === 'Escape') {
-    reset()
-  }
-
-  if (key === ',') {
-    if (
-      !storedNumber.includes('.') &&
-      storedNumber.length < MAX_DIGITS_IN_DISPLAY
-    ) {
-      storedNumber += '.'
-    }
-    setDisplay(storedNumber)
+  switch (key) {
+    case '1':
+      appendNumber(1)
+      break
+    case '2':
+      appendNumber(2)
+      break
+    case '3':
+      appendNumber(3)
+      break
+    case '4':
+      appendNumber(4)
+      break
+    case '5':
+      appendNumber(5)
+      break
+    case '6':
+      appendNumber(6)
+      break
+    case '7':
+      appendNumber(7)
+      break
+    case '8':
+      appendNumber(8)
+      break
+    case '9':
+      appendNumber(9)
+      break
+    case '0':
+      appendNumber(0)
+      break
+    case ',':
+      appendDot()
+      break
+    case 'Escape':
+      reset()
+      break
+    case 'Control':
+      negate()
+      break
+    case 'sum':
+      updateOperation('sum')
+      break
+    case 'subtract':
+      updateOperation('subtract')
+      break
+    case 'multiply':
+      updateOperation('multiply')
+      break
+    case 'divide':
+      updateOperation('divide')
+      break
+    case 'equal':
+      executeOperation(operation)
+      break
+    default:
+      break
   }
 })
 
