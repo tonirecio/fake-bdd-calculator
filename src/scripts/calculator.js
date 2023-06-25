a9af0995839b6871d3c264133cf1c3ecb3ce323f
Anas Lharrak <anlhbiada@gmail.com>
2023-06-25 16:03:52 +0200

    Postrefactor 2

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,199 +1,96 @@
-
-
 const MAX_DIGITS_IN_DISPLAY = 10
+const display = document.querySelector('div[name="display"] span')
+
 let inputValue = null
 let valueDisplay = ""
 let firstNumber = null
 let operator = null
 let secondNumber = null
 let isSecondNumber = false
-let comma = false
-let tryNegateSecondNumber = false
-let tryNegateFirstNumber = false
-let arrOperations = []
-const display = document.querySelector('div[name="display"] span')
-
-const getValueDisplay = (value) => {
-    value = value.toString()
-    value = value.replace(".", ",")
-    return value
-}
+let tryingNegateNumber = false
 
 const concatInputsNumbers = (value) => {
-  if(tryNegateFirstNumber === false && !isSecondNumber){
-    inputValue = inputValue.toString()
-    inputValue = inputValue + value
-    setDisplay(inputValue)
-    inputValue = parseFloat(inputValue)
-  }
-  else if(isSecondNumber){
-    if(secondNumber === null){
-      secondNumber = value
-      valueDisplay = secondNumber
-      setDisplay(valueDisplay)
-    }
-    else{
-      if(tryNegateSecondNumber === false){
-        valueDisplay = secondNumber
-        valueDisplay = valueDisplay.toString() + value
-        secondNumber = parseFloat(valueDisplay, 10) 
-        inputValue = parseFloat(valueDisplay, 10) 
-        setDisplay(valueDisplay)
-      }
-      else{
-        
-        valueDisplay = inputValue
-        
-        valueDisplay = valueDisplay.toString() + value
-        
-        setDisplay(value)
-        tryNegateSecondNumber = false
-        return
-      }
-      
-
-return
-    }
-  }
-}
-
-const putCommaAndConcatNumbers = (value) => {
-  if(tryNegateFirstNumber === false){
-    inputValue = inputValue.toString()
-    inputValue = inputValue + "." + value
-    setDisplay(inputValue)
-    inputValue = parseFloat(inputValue)
-  }
-  else{
+  if(tryingNegateNumber === true){
     inputValue = value
     setDisplay(inputValue)
-    inputValue = parseFloat(inputValue)
-  }
+    return
+  } else {
+      valueDisplay = inputValue
+      valueDisplay = valueDisplay.toString() + value
+      inputValue = parseFloat(valueDisplay, 10)
+      setDisplay(valueDisplay)
+      return
+  } 
+}
   
+const putCommaAndConcatNumbers = (value) => {
+  valueDisplay = inputValue
+  valueDisplay = valueDisplay.toString()
+  if(tryingNegateNumber == false){
+    valueDisplay = valueDisplay + "." + value
+  } else {
+    valueDisplay = valueDisplay + "."
+  }
+  inputValue = parseFloat(valueDisplay, 10)
+  setDisplay(valueDisplay)
 }
 
-const negateInputValue = (value) => {
-    
-    comma = false
-    if(!isSecondNumber){
-      tryNegateFirstNumber = true
-    }
-    else{
-      tryNegateSecondNumber = true
-    }
-
-    if(valueDisplay.slice(-1) === ","){
-      comma = true
-    }
-    
-    if(tryNegateFirstNumber === true){
-      
-      
-      value = value * -1
-      inputValue = value
-      return
-    }
-    else if(tryNegateSecondNumber === true){
-      
-      
-      value = value * -1 
-      inputValue = value
-      return
-    }
+const negateInputValue = (valueToNegate) => {
+  if(valueDisplay.slice(-1) === ','){
+    pendingComma = true
+  }
+  tryingNegateNumber = true
+  inputValue = valueToNegate * -1
+  setInputValue(inputValue)
+  return
 }
 
-const setInputValue = (value) => {
-  
-  if(isSecondNumber === true){
-      inputValue = value
-      concatInputsNumbers(inputValue)
-      return
-  }
-  
-
-  if((valueDisplay.replace(",", "")).length + 1 > MAX_DIGITS_IN_DISPLAY && tryNegateFirstNumber === false){
+const setInputValue = (input) => {
+  if((valueDisplay.replace(",", "")).length + 1 > MAX_DIGITS_IN_DISPLAY && tryingNegateNumber === false){
     return
-  }
-  else if(value === "." && valueDisplay.includes(",") && isSecondNumber === false){
+  } else if(input === "." && valueDisplay.includes(",")){
     return
-  }
-  else if(inputValue === 0 && value != "." && !valueDisplay.includes(",") && isSecondNumber === false){
-    inputValue = value;
-  }
-  else if(inputValue === 0 && value === "."){
-    
-    valueDisplay = "0,"
-    setDisplay(valueDisplay)
+  } else if(inputValue === 0 && input != "." && !valueDisplay.includes(",")){
+    inputValue = input;
+    setDisplay(inputValue)
     return
-  }
-  else{
+  } else {
     if(valueDisplay.slice(-1) === ","){
-      
-      putCommaAndConcatNumbers(value)
+      putCommaAndConcatNumbers(input)
       return
-    }
-    else if(isSecondNumber === false && tryNegateFirstNumber === false){
-      concatInputsNumbers(value)
+    } else {
+      concatInputsNumbers(input)
       return
-    }
-    else{
-      inputValue = value
-    }
+    } 
   }
-  
-  setDisplay(inputValue)
-  
 }
 
 const setDisplay = (value) => {
-  
-  if(firstNumber != null){
-      valueDisplay = firstNumber + operator
-    }
-
   valueDisplay = value.toString().replace(".", ",")
-  
   if(isSecondNumber === false){
-    if(comma === true){
-      valueDisplay = valueDisplay + ","
-    }  
-    
     display.innerHTML = valueDisplay
-   
-  }
-  else if(isSecondNumber === true){
-      
-      secondNumber = value
-      valueDisplay = secondNumber
-      valueDisplay = valueDisplay.toString().replace(".", ",")
-      
+  } else {
+      secondNumber = parseFloat(value, 10)
       display.innerHTML = valueDisplay.replace(".", ",")
-   
-    
   }
 }
 
 const resetDisplay = () => {
   inputValue = 0
   valueDisplay = '0'
-  comma = false
-  display.innerHTML = "0"
+  display.innerHTML = valueDisplay
 }
 
 handleOperator = (operation) => {
   firstNumber = inputValue
   operator = operation
   valueForDisplay = operator
-  setDisplay(operator)
+  setDisplay(valueForDisplay)
   isSecondNumber = true
-  
+  inputValue = 0
 }
 
 const handleOperation = () => {
-  if(tryNegateSecondNumber === false){
-    secondNumber = inputValue
-  }
-
   switch (operator){
     case '+':
       result = sumNumbers(firstNumber, secondNumber)
@@ -211,38 +108,25 @@ const handleOperation = () => {
       result = divideNumbers(firstNumber, secondNumber)
       break;
   } 
-
-      console.log(firstNumber)
-      console.log(operator)
-      console.log(secondNumber)
-      console.log(result)
-
-      arrOperations.push(secondNumber)
-      isSecondNumber = false
-      tryNegateSecondNumber = false
-      inputValue = result
-      valueDisplay = ""
-      valueDisplay = result
-      valueDisplay = valueDisplay.toString()
+    
+  isSecondNumber = false
+  tryingNegateNumber = false
+  inputValue = result
+  valueDisplay = result
+  valueDisplay = valueDisplay.toString()
       
-      
-      if(valueDisplay.includes(".")){
-        valueDisplay.replace(".", "")
-        lengthA = valueDisplay.length 
-        if(lengthA > 10){
-          lengthA = 10
-        }
-        
-        console.log(lengthA)
-        result = result.toPrecision(lengthA) * 1
-      }
-      
-
-      setDisplay(result)
-      firstNumber = null
-      operator = null
-      secondNumber = null
-      
+  if(valueDisplay.includes(".")){
+    valueDisplay.replace(".", "")
+    lengthOfResult = valueDisplay.length 
+    if(lengthOfResult > 10){
+        lengthOfResult = 10
+    }      
+    result = result.toPrecision(lengthOfResult) * 1
+  }
+  setDisplay(result)
+  firstNumber = null
+  operator = null
+  secondNumber = null   
 }
 
 sumNumbers = (firstNumber, secondNumber) => {
@@ -298,10 +182,8 @@ writeNumber = () => {
     setInputValue('.')
   })
   document.getElementsByName('negate')[0].addEventListener('click', () => {
-        negateInputValue(inputValue)
-        setInputValue(inputValue)
-        tryNegateFirstNumber = false
-
+    negateInputValue(inputValue)
+    tryingNegateNumber = false
   })
   document.getElementsByName('divide')[0].addEventListener('click', () => {
     handleOperator("/")
@@ -326,8 +208,7 @@ writeNumber = () => {
     }
     else if (event.key === "Control") {
       negateInputValue(inputValue)
-      setInputValue(inputValue)
-      tryNegateFirstNumber = false
+      tryingNegateNumber = false
     }
     arrKeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, ','];
     arrKeys.forEach(num => {
