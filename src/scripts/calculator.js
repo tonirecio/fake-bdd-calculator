7aa9550c2302ddb1509c205c823cd699cecb9b5f
gdg2004396 <113702199+gdg2004396@users.noreply.github.com>
2023-06-26 11:01:20 +0200

    Scenario Outline: Performing two number operations & Fix Code

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,117 +1,48 @@
+// yarn test tests/features/calculator.feature --tags "@test1Done"
+
 const MAX_DIGITS_IN_DISPLAY = 10
-let hasDecimal = false
+let firstOperator
+let currentValue = '0'
+let currentValueOperator
+const DecimalPoint = ','
+let refreshDisplay = false
 
 const setDisplay = (value) => {
-  display.innerHTML = value
+  const display = document.querySelector('div[name="display"] span')
+  const updatedValue = value.replace('.', DecimalPoint)
+  display.innerHTML = updatedValue
 }
 
 const reset = () => {
-  setDisplay(0)
-  hasDecimal = false
+  firstOperator = 0
+  currentValue = '0'
+  refreshDisplay = false
+  currentValueOperator = null
+  setDisplay(currentValue)
 }
 
-const DisplayAcu = (value) => {
-  const currentValue = display.innerHTML
-
-  if (value === '+' || value === '-') {
-    if (currentValue === '0') {
-      display.innerHTML = value
-    } else if (currentValue === '+' || currentValue === '-') {
-      display.innerHTML = value === currentValue ? value : value === '+' ? '-' : '+'
-    } else {
-      const newValue = currentValue + value
-      display.innerHTML = newValue
-    }
+const pressNumber = (buttonNumber) => {
+  if (refreshDisplay) {
+    currentValue = buttonNumber.toString()
+    refreshDisplay = false
   } else {
-    const newValue = currentValue === '0' && value !== ',' ? value : currentValue + value
-    if (newValue.replace(',', '').replace('+', '').replace('-', '').length <= MAX_DIGITS_IN_DISPLAY) {
-      display.innerHTML = newValue
-    } else {
-      window.alert('Hello. The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.')
-      // reset();
+    if (
+      currentValue.length < MAX_DIGITS_IN_DISPLAY ||
+      (currentValue.includes('.') && 
+        currentValue.length <= MAX_DIGITS_IN_DISPLAY)
+    ) {
+      if (currentValue !== '0') {
+        currentValue += buttonNumber
+      } else {
+        currentValue = buttonNumber.toString()
+      }
     }
   }
+  setDisplay(currentValue)
 }
-
-document.getElementsByName('clean')[0].addEventListener('click', () => {
-  reset()
-})
-
-const display = document.querySelector('div[name="display"] span')
-document.getElementsByName('multiply')[0].addEventListener('click', () => {
-  DisplayAcu('x')
-})
-
-const displaysum = document.querySelector('div[name="display"] span')
-document.getElementsByName('sum')[0].addEventListener('click', () => {
-  DisplayAcu('+')
-})
-
-const displaysubtract = document.querySelector('div[name="display"] span')
-document.getElementsByName('subtract')[0].addEventListener('click', () => {
-  DisplayAcu('-')
-})
-
-const displaydivide = document.querySelector('div[name="display"] span')
-document.getElementsByName('divide')[0].addEventListener('click', () => {
-  DisplayAcu('÷')
-})
-
-const displaypoint = document.querySelector('div[name="display"] span')
-document.getElementsByName('point')[0].addEventListener('click', () => {
-  if (!hasDecimal) {
-    Coma(',')
-  }
-})
-
-document.getElementsByName('negate')[0].addEventListener('click', () => {
-  negateInvertNumber()
-})
-
-document.getElementsByName('zero')[0].addEventListener('click', () => {
-  DisplayAcu('0')
-})
-
-document.getElementsByName('one')[0].addEventListener('click', () => {
-  DisplayAcu('1')
-})
-
-document.getElementsByName('two')[0].addEventListener('click', () => {
-  DisplayAcu('2')
-})
-
-document.getElementsByName('three')[0].addEventListener('click', () => {
-  DisplayAcu('3')
-})
-
-document.getElementsByName('four')[0].addEventListener('click', () => {
-  DisplayAcu('4')
-})
-
-document.getElementsByName('five')[0].addEventListener('click', () => {
-  DisplayAcu('5')
-})
-
-document.getElementsByName('six')[0].addEventListener('click', () => {
-  DisplayAcu('6')
-})
-
-document.getElementsByName('seven')[0].addEventListener('click', () => {
-  DisplayAcu('7')
-})
-
-document.getElementsByName('eight')[0].addEventListener('click', () => {
-  DisplayAcu('8')
-})
-
-document.getElementsByName('nine')[0].addEventListener('click', () => {
-  DisplayAcu('9')
-})
 
 const negateInvertNumber = () => {
-  let currentValue = display.innerHTML
-
-  if (currentValue !== '0' && currentValue !== '0,') {
+  if (currentValue !== '0' && currentValue !== '0.') {
     if (currentValue.startsWith('-')) {
       currentValue = currentValue.slice(1)
     } else if (currentValue.startsWith('+')) {
@@ -123,77 +54,120 @@ const negateInvertNumber = () => {
   }
 }
 
-const Coma = (value) => {
-  const currentValue = display.innerHTML
-  if (value === ',') {
-    if (!hasDecimal && currentValue.indexOf(',') === -1 && currentValue.length < MAX_DIGITS_IN_DISPLAY) {
-      const newValue = currentValue === '0' ? '0' + value : currentValue + value
-      display.innerHTML = newValue
-    }
+const floatCurrentValue = () => {
+  if (
+    !currentValue.includes('.') &&
+    currentValue.length < MAX_DIGITS_IN_DISPLAY
+  ) {
+    currentValue += '.'
   }
+  setDisplay(currentValue)
 }
 
-const pressKey = (event) => {
-  const key = event.key
+const concludeOperation = () => {
+  const secondOperator = parseFloat(currentValue)
+  let outcome = null
+  switch (currentValueOperator) {
+    case 'sum':
+      outcome = firstOperator + secondOperator
+      break
+    case 'subtract':
+      outcome = firstOperator - secondOperator
+      break
+    case 'multiply':
+      outcome = firstOperator * secondOperator
+      break
+    case 'divide':
+      outcome = firstOperator / secondOperator
+      break
+    default:
+      console.log(
+        '[ERROR] ' +
+          currentValueOperator +
+          ' The implementation is still pending.'
+      )
+  }
+  outcome = parseFloat(outcome.toPrecision(MAX_DIGITS_IN_DISPLAY))
+  setDisplay(outcome.toString())
+}
 
-  switch (key) {
-    case '+':
-      DisplayAcu('+')
-      break
-    case '-':
-      DisplayAcu('-')
-      break
-    case '*':
-      DisplayAcu('x')
-      break
-    case '/':
-      DisplayAcu('÷')
-      break
-    case '.':
-    case ',':
-      if (!hasDecimal) {
-        DisplayAcu(',')
+const recordFunctionButtonPress = (buttonIdentifier, assignedFunction) => {
+  document
+    .getElementsByName(buttonIdentifier)[0]
+    .addEventListener('click', assignedFunction)
+}
+
+const recordOperationButtonPress = (buttonIdentifier) => {
+  document
+    .getElementsByName(buttonIdentifier)[0]
+    .addEventListener('click', () => {
+      if (currentValueOperator !== null) {
+        concludeOperation()
       }
-      break
-    case '0':
-      DisplayAcu('0')
-      break
-    case '1':
-      DisplayAcu('1')
-      break
-    case '2':
-      DisplayAcu('2')
-      break
-    case '3':
-      DisplayAcu('3')
-      break
-    case '4':
-      DisplayAcu('4')
-      break
-    case '5':
-      DisplayAcu('5')
-      break
-    case '6':
-      DisplayAcu('6')
-      break
-    case '7':
-      DisplayAcu('7')
-      break
-    case '8':
-      DisplayAcu('8')
-      break
-    case '9':
-      DisplayAcu('9')
-      break
+      currentValueOperator = buttonIdentifier
+      firstOperator = parseFloat(currentValue.replace(',', '.'))
+      refreshDisplay = true
+    })
+}
+
+recordFunctionButtonPress('clean', reset)
+recordFunctionButtonPress('equal', concludeOperation)
+recordFunctionButtonPress('point', floatCurrentValue)
+recordFunctionButtonPress('negate', negateInvertNumber)
+recordOperationButtonPress('multiply')
+recordOperationButtonPress('subtract')
+recordOperationButtonPress('divide')
+recordOperationButtonPress('sum')
+
+document.getElementsByName('zero')[0].addEventListener('click', () => {
+  pressNumber(0)
+})
+document.getElementsByName('one')[0].addEventListener('click', () => {
+  pressNumber(1)
+})
+document.getElementsByName('two')[0].addEventListener('click', () => {
+  pressNumber(2)
+})
+document.getElementsByName('three')[0].addEventListener('click', () => {
+  pressNumber(3)
+})
+document.getElementsByName('four')[0].addEventListener('click', () => {
+  pressNumber(4)
+})
+document.getElementsByName('five')[0].addEventListener('click', () => {
+  pressNumber(5)
+})
+document.getElementsByName('six')[0].addEventListener('click', () => {
+  pressNumber(6)
+})
+document.getElementsByName('seven')[0].addEventListener('click', () => {
+  pressNumber(7)
+})
+document.getElementsByName('eight')[0].addEventListener('click', () => {
+  pressNumber(8)
+})
+document.getElementsByName('nine')[0].addEventListener('click', () => {
+  pressNumber(9)
+})
+
+document.addEventListener('keyup', (event) => {
+  const keyName = event.key
+
+  switch (keyName) {
     case 'Control':
       negateInvertNumber()
       break
     case 'Escape':
       reset()
       break
-    default:
+    case DecimalPoint:
+      floatCurrentValue(keyName)
       break
+    default:
+      if (/[0-9]/.test(keyName)) {
+        pressNumber(Number(keyName))
+      }
   }
-}
+})
 
-document.addEventListener('keydown', pressKey)
+reset()
--- tests/features/calculator.feature
+++ tests/features/calculator.feature
@@ -95,7 +95,7 @@ Examples:
 |    123456789, |      5 |   123456789,5 |
 |   123456789,5 |     +- |  -123456789,5 |
 
-@test6
+@test6Done
 Scenario Outline: Performing two number operations
 Given the user writes the following value: "<displayNumber>"
 And the user presses the "<button>" button
@@ -128,6 +128,7 @@ Examples:
 |           -10 |      / |          2 |            -5 |
 |           -10 |      / |         -2 |             5 |
 
+@test7Done
 Scenario Outline: Before clicking the equal button
 Given the user writes the following value: "<displayNumber>"
 And the user presses the "<button>" button
@@ -141,6 +142,7 @@ Examples:
 |         13,14 |      * |      2,781 |
 |            84 |      / |       -4,3 |
 
+@test8
 Scenario Outline: Performing two number operations with a result number with more than 10 nondecimal digits
 Given the user writes the following value: "<displayNumber>"
 And the user presses the "<button>" button
