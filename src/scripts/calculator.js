a14070a90a1be65fe659614ab1591a013bec791a
cristinaco32 <cristinacosiallsibiza@gmail.com>
2023-06-26 10:10:22 +0200

    Pressing non-operators keys

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -4,75 +4,163 @@ const setDisplay = (value) => {
   display.innerHTML = value
 }
 
-const sayHello = () => {
-  window.alert('Hello. The maximum number of digits in the display is ' + MAX_DIGITS_IN_DISPLAY + '.')
-}
-
-const updateDisplay = (value) => {
-  setDisplay(value)
-}
-
-let currentValue
-
-const reset = () => {
-  currentValue = ''
-  updateDisplay(0)
-}
+let currentValue = 0
+let previousOperand
+let countDigit = 0
+let isDecimal = false
+//let number
+//let operator
+//isnextnumberdecimal
+//isdisplaytobecleaned
 
 const display = document.querySelector('div[name="display"] span')
-document.getElementsByName('multiply')[0].addEventListener('click', () => {
-  sayHello()
+
+document.getElementsByName('zero')[0].addEventListener('click', () =>{
+  addNumberButtons(0)
 })
-reset()
-
-//const charButtons = document.querySelectorAll('[aria-label]')
-
-const numberButtons = document.querySelectorAll('button:not([aria-label])')
-numberButtons.forEach(numberButton =>{
-  numberButton.addEventListener('click', () => {
-    setDisplay(numberButton.textContent)
-  })
+document.getElementsByName('one')[0].addEventListener('click', () =>{
+  addNumberButtons(1)
 })
-
+document.getElementsByName('two')[0].addEventListener('click', () =>{
+  addNumberButtons(2)
+})
+document.getElementsByName('three')[0].addEventListener('click', () =>{
+  addNumberButtons(3)
+})
+document.getElementsByName('four')[0].addEventListener('click', () =>{
+  addNumberButtons(4)
+})
+document.getElementsByName('five')[0].addEventListener('click', () =>{
+  addNumberButtons(5)
+})
+document.getElementsByName('six')[0].addEventListener('click', () =>{
+  addNumberButtons(6)
+})
+document.getElementsByName('seven')[0].addEventListener('click', () =>{
+  addNumberButtons(7)
+})
+document.getElementsByName('eight')[0].addEventListener('click', () =>{
+  addNumberButtons(8)
+})
+document.getElementsByName('nine')[0].addEventListener('click', () =>{
+  addNumberButtons(9)
+})
 document.getElementsByName('clean')[0].addEventListener('click', () => {
   reset()
 })
-/*
-numberButtons.forEach(numberButton => {
-  numberButton.addEventListener('click', () =>{
-  if(parseInt(currentValue) === 0){
-    currentValue = numberButton.textContent
-  }
-  else {
-    currentValue += numberButton.textContent
-  }
-  setDisplay(currentValue)
-  })
+document.getElementsByName('negate')[0].addEventListener('click', () => {
+  negateButton(currentValue)
 })
-*/
+document.getElementsByName('divide')[0].addEventListener('click', () => {
 
-numberButtons.forEach(numberButton => {
-  numberButton.addEventListener('click', () =>{
-  if(currentValue != 0  || numberButton.textContent == ',' ){
-    currentValue += numberButton.textContent
-  }
-  else {
-    currentValue = numberButton.textContent
-  }
-  setDisplay(currentValue)
-  })
+})
+document.getElementsByName('multiply')[0].addEventListener('click', () => {
+  
+})
+document.getElementsByName('sum')[0].addEventListener('click', () => {
+  operatorButtons('+')
+})
+document.getElementsByName('subtract')[0].addEventListener('click', () => {
+  
+})
+const buttonPointName = document.getElementsByName('point')[0].addEventListener('click', () => {
+  operatorButtons('point')
+})
+document.getElementsByName('equal')[0].addEventListener('click', () => {
+  
 })
 
-const negateButton = (displayValues) => {
-  displayValues = displayValues * -1
-  updateDisplay(displayValues)
+const addNumberButtons = (number) =>{
+  appendNumbers(number)
+  setDisplay(currentValue)
 }
 
-document.getElementsByName('negate')[0].addEventListener('click', () => {
-  negateButton(display.innerHTML)
-})
+const reset = () => {
+  currentValue = 0
+  previousOperand = 0
+  setDisplay(0)
+}
 /*
-document.getElementsByName('point')[0].addEventListener('click', () => {
-  displayValues()
-})
+const appendNumbers = (value) => {
+  if(countDigit <= MAX_DIGITS_IN_DISPLAY){
+    if(currentValue != 0 ){
+      console.log(typeof currentValue)
+      currentValue = currentValue * 10 + value
+    }
+    else {
+      currentValue = value
+    }
+    countDigit++;
+  }
+  else {
+    window.alert("3")
+  }
+}
 */
+const appendNumbers = (value) => {
+  if (countDigit <= MAX_DIGITS_IN_DISPLAY) {
+    if (currentValue !== 0) {
+      // Verificar si ya hay una coma en el valor actual
+      if (!isDecimal) {
+        currentValue = currentValue * 10 + value;
+      } else {
+        // Calcular la cantidad de dígitos después de la coma
+        const decimalDigits = currentValue.toString().split('.')[1];
+        const decimalMultiplier = Math.pow(10, decimalDigits.length);
+        const newValue = currentValue + value / decimalMultiplier;
+        currentValue = parseFloat(newValue.toFixed(decimalDigits.length));
+      }
+    } else {
+      currentValue = value;
+    }
+    countDigit++;
+    setDisplay(currentValue);
+  } else {
+    window.alert("3");
+  }
+};
+ 
+const negateButton = (displayValue) => {
+  displayValue = displayValue * -1
+  currentValue = displayValue
+  setDisplay(displayValue)
+}
+
+//const buttons = document.querySelector('keypad')
+/*
+const operatorButtons = (buttonPointName) => {
+  if (buttonPointName == 'point') {
+    setDisplay(currentValue + ',')
+    console.log(typeof currentValue)
+  }
+};
+*/
+const operatorButtons = (buttonPointName) => {
+  if (buttonPointName === 'point') {
+    if (!isDecimal) {
+      setDisplay(currentValue + ',');
+      isDecimal = true;
+    }
+  }
+};
+
+/*keydown numbers, escape, control and comma*/
+
+document.addEventListener("keydown",(event)=> {
+    let pressedKey = event.key
+    if(pressedKey >= '0' && pressedKey <= '9' || pressedKey == ','){
+      if(currentValue != 0 || pressedKey.toString().includes(',')){
+        currentValue += pressedKey 
+      }
+      else {
+        currentValue = pressedKey
+      }
+      setDisplay(currentValue)
+  
+    }
+  if(pressedKey === "Escape"){
+    reset()
+  }else if(pressedKey === "Control"){
+    negateButton(currentValue)
+  }
+})
--- tests/features/calculator.feature
+++ tests/features/calculator.feature
@@ -6,7 +6,7 @@ Given a user opens the app
 Scenario: Default display screen
 Then the display should show the following value: "0"
 
-@wip
+@done
 Scenario Outline: Pressing non-operators screen buttons
 Given the user writes the following value: "<displayNumber>"
 When the user presses the "<button>" button
@@ -29,6 +29,7 @@ Examples:
 |             1 |     +- |            -1 |
 |            -1 |     +- |             1 |
 
+@wip
 Scenario Outline: Pressing non-operators keys
 Given the user writes the following value: "<displayNumber>"
 When the user presses the "<key>" key
